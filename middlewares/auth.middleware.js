8714cca09869a36b92dcd80f60e44315721aafde
sanaf3739 <sanaf6350@gmail.com>
2025-04-11 11:35:33 +0530

    made some changes in auth middleware for debugging

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -2,10 +2,12 @@ const jwt = require("jsonwebtoken")
 const authMiddleware = (req, res, next) => {
   try {
     const token = req.cookies?.accessToken || req.headers?.authorization?.split(" ")[1];
+    console.log(token)
     if (!token) return res.status(401).json({success: false, error: "unauthorized"});
     try {
       const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
       req.user = decoded;
+      console.log(user)
       return next();
     } catch (err) {
       console.log(err);
